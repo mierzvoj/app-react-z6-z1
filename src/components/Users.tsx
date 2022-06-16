a36fa1a48b7bca4dbabd2b6072e34ee2acd3f42a
mierzvoj <mierzvoj@gmail.com>
2022-06-16 18:04:15 +0200

    first commit

---
+++ src/components/Users.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { List } from "../interfaces/DataInterface";
+import { Person } from "./Person";
+import "./Users.css";
+
+export const Users = () => {
+  const [users, setUsers] = useState<List>();
+
+  const fetchUsers: () => any = () => {
+    fetch("https://randomuser.me/api/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="usersContainer">
+      {!!users?.info.seed &&
+        users?.results.map((user, index) => <Person {...user} key={index} />)}
+      <button onClick={fetchUsers} className="button">
+        Show next user
+      </button>
+    </div>
+  );
+};
